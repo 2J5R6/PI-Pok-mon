49fee16f97d4e79577c08f7c8e69f8f1e836e3f0
Julián Andrés Rosas Sánchez <julianrosassan@gmail.com>
2023-10-26 15:53:56 -0500

    First commit, models and empty controllers´folder

---
+++ api/src/models/Pokemon.js
@@ -0,0 +1,67 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+  sequelize.define('pokemon', {
+    // ID del Pokemon
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true
+    },
+
+    // Nombre del Pokemon
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+
+    // Imagen del Pokemon
+    image: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+
+    // Vida del Pokemon
+    hp: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+
+    // Ataque del Pokemon
+    attack: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+
+    // Defensa del Pokemon
+    defense: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+
+    // Velocidad del Pokemon
+    speed: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+
+    // Altura del Pokemon
+    height: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+
+    // Peso del Pokemon
+    weight: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+
+    // Descripción (opcional, basado en la sugerencia)
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: true
+    }
+  });
+};
---
+++ api/src/models/Type.js
@@ -0,0 +1,27 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+  sequelize.define('type', {
+    // ID del Tipo
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true
+    },
+
+    // Nombre del Tipo
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    }
+  }, {
+    // Opciones adicionales para el modelo
+    indexes: [
+      {
+        unique: true,
+        fields: ['name']
+      }
+    ]
+  });
+};
