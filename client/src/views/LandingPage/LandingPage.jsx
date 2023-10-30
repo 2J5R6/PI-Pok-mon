66cecf404197a624bf063455ce6cda2b8bf442d1
Julián Andrés Rosas Sánchez <julianrosassan@gmail.com>
2023-10-30 06:42:34 -0500

    Fixing Landing

--- client/src/views/LandingPage/LandingPage.jsx
+++ client/src/views/LandingPage/LandingPage.jsx
@@ -1,18 +1,14 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import styles from './LandingPage.module.css';
-import pokemonImage from '../../assets/images/Pokémon-Gotta.png'
 
 const LandingPage = () => {
-  const navigate = useNavigate();
-
   return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>Bienvenido al Mundo Pokémon</h1>
-      <img src={pokemonImage} alt="Imagen representativa de PokémonWeb" className={styles.image} />
-      <button className={styles.exploreButton} onClick={() => navigate('/home')}>Explorar Pokémons</button>
+    <div className={styles.landingContainer}>
+      <h1 className={styles.title}>Created by Julian Rosas</h1>
+      <Link to="/home" className={styles.exploreButton}>Explorar Pokémons</Link>
     </div>
   );
-}
+};
 
 export default LandingPage;
