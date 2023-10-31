c2e870bc68ff9a3ff3f33284fed79784cf07b164
Julián Andrés Rosas Sánchez <julianrosassan@gmail.com>
2023-10-30 22:52:44 -0500

    Searchbar

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -19,20 +19,21 @@ const SearchBar = () => {
   };
 
   return (
-    <form onSubmit={handleSearch} className={styles.searchForm}>
-      <input
-        type="text"
-        placeholder="Buscar Pokemon..."
-        value={searchTerm}
-        onChange={handleInputChange}
-        className={styles.searchInput}
-      />
-      <button type="submit" className={styles.searchButton}>
-        Buscar
-      </button>
-    </form>
+    <div className={styles.searchContainer}>
+      <form onSubmit={handleSearch} className={styles.searchForm}>
+        <input
+          type="text"
+          placeholder="Buscar Pokemon..."
+          value={searchTerm}
+          onChange={handleInputChange}
+          className={styles.searchInput}
+        />
+        <button type="submit" className={styles.searchButton}>
+          Buscar
+        </button>
+      </form>
+    </div>
   );
 };
 
 export default SearchBar;
-
