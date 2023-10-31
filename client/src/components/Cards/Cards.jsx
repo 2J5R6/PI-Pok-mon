61734becffa4bf7bb0c993ccc3820e11b69faff6
Julián Andrés Rosas Sánchez <julianrosassan@gmail.com>
2023-10-31 07:14:33 -0500

    Image and  Cards

--- client/src/components/Cards/Cards.jsx
+++ client/src/components/Cards/Cards.jsx
@@ -1,27 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Card from '../Card/Card';
 import Pagination from '../Pagination/Pagination';
 import styles from './Cards.module.css';
-import { setCurrentPage } from '../../redux/actions/pokemonActions';
+import { setCurrentPage, getPokemons } from '../../redux/actions/pokemonActions';
 
 const Cards = () => {
   const dispatch = useDispatch();
-  const pokemons = useSelector(state => state.pokemons);
-  const searchedPokemon = useSelector(state => state.searchedPokemon);
+  const pokemons = useSelector(state => state.pokemons) || [];
+  const searchedPokemon = useSelector(state => state.searchedPokemon) || [];
   const currentPage = useSelector(state => state.currentPage);
   const totalPages = useSelector(state => state.totalPages);
+
+  const [randomPokemons, setRandomPokemons] = useState([]);
+
+  useEffect(() => {
+    if (!pokemons.length) {
+      dispatch(getPokemons());
+    } else {
+      const shuffled = pokemons.sort(() => 0.5 - Math.random());
+      setRandomPokemons(shuffled.slice(0, 3));
+    }
+  }, [pokemons, dispatch]);
 
   const handlePageChange = (pageNumber) => {
     dispatch(setCurrentPage(pageNumber));
   };
 
-  const displayPokemons = (searchedPokemon && searchedPokemon.length) ? searchedPokemon : pokemons;
+  const displayPokemons = searchedPokemon.length ? searchedPokemon : randomPokemons;
 
-if (!displayPokemons || !displayPokemons.length) {
-  return <p className={styles.errorMessage}>No se encontraron pokemons. Por favor, intenta nuevamente.</p>;
-}
-
+  if (!displayPokemons.length) {
+    return <p className={styles.errorMessage}>No se encontraron pokemons. Por favor, intenta nuevamente.</p>;
+  }
 
   return (
     <div className={styles.cardsContainer}>
@@ -40,4 +50,3 @@ if (!displayPokemons || !displayPokemons.length) {
 const MemoizedCard = React.memo(Card);
 
 export default Cards;
-
