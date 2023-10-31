70bacb72df43c28882d90b4eb594b459de5efa48
Julián Andrés Rosas Sánchez <julianrosassan@gmail.com>
2023-10-31 11:17:49 -0500

    Filters

--- client/src/components/Filters/Filters.jsx
+++ client/src/components/Filters/Filters.jsx
@@ -1,46 +1,71 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { filterByType, orderBy } from '../../redux/actions/pokemonActions';
-import styles from './Filter.module.css';
+import { filterByType, filterBySource, sortBy } from '../../redux/actions/pokemonActions';
+import styles from './Filters.module.css';
 
 const Filters = () => {
-  const [selectedType, setSelectedType] = useState('');
-  const [order, setOrder] = useState('');
   const dispatch = useDispatch();
+  const [selectedType, setSelectedType] = useState('');
+  const [selectedSource, setSelectedSource] = useState('db');
+  const [selectedSort, setSelectedSort] = useState('');
+
+  const types = [
+    'Normal', 'Fighting', 'Flying', 'Poison', 'Ground', 'Rock',
+    'Bug', 'Ghost', 'Steel', 'Fire', 'Water', 'Grass',
+    'Electric', 'Psychic', 'Ice', 'Dragon', 'Dark',
+    'Fairy', 'Unknown', 'Shadow'
+  ];
 
   const handleTypeChange = (e) => {
     setSelectedType(e.target.value);
-    dispatch(filterByType(e.target.value));
   };
 
-  const handleOrderChange = (e) => {
-    setOrder(e.target.value);
-    dispatch(orderBy(e.target.value));
+  const handleSourceChange = (e) => {
+    setSelectedSource(e.target.value);
   };
 
-  const pokemonTypes = [
-    'normal', 'fighting', 'flying', 'poison', 'ground', 'rock', 'bug', 'ghost', 'steel', 'fire',
-    'water', 'grass', 'electric', 'psychic', 'ice', 'dragon', 'dark', 'fairy', 'unknown', 'shadow'
-  ];
+  const handleSortChange = (e) => {
+    setSelectedSort(e.target.value);
+  };
+
+  const applyFilters = () => {
+    if (selectedType) {
+      dispatch(filterByType(selectedType));
+    }
+    dispatch(filterBySource(selectedSource));
+    if (selectedSort) {
+      dispatch(sortBy(selectedSort));
+    }
+  };
 
   return (
-    <div className={styles.filterContainer}>
-      <label>Filtrar por Tipo:</label>
-      <select value={selectedType} onChange={handleTypeChange}>
-        <option value="">Todos</option>
-        {pokemonTypes.map(type => (
-          <option key={type} value={type}>{type}</option>
-        ))}
-      </select>
-
-      <label>Ordenar por:</label>
-      <select value={order} onChange={handleOrderChange}>
-        <option value="">Seleccionar</option>
-        <option value="id-asc">ID Ascendente</option>
-        <option value="id-desc">ID Descendente</option>
-        <option value="name-asc">Nombre A-Z</option>
-        <option value="name-desc">Nombre Z-A</option>
-      </select>
+    <div className={styles.filtersContainer}>
+      <h2 className={styles.filterTitle}>Filters</h2>
+      <div className={styles.filterGroup}>
+        <label className={styles.filterLabel}>Type:</label>
+        <select value={selectedType} onChange={handleTypeChange}>
+          <option value="">All</option>
+          {types.map(type => (
+            <option key={type} value={type}>{type}</option>
+          ))}
+        </select>
+      </div>
+      <div className={styles.filterGroup}>
+        <label className={styles.filterLabel}>Source:</label>
+        <select value={selectedSource} onChange={handleSourceChange}>
+          <option value="db">Database</option>
+          <option value="api">API</option>
+        </select>
+      </div>
+      <div className={styles.filterGroup}>
+        <label className={styles.filterLabel}>Sort By:</label>
+        <select value={selectedSort} onChange={handleSortChange}>
+          <option value="">None</option>
+          <option value="id">ID</option>
+          <option value="name">Name</option>
+        </select>
+      </div>
+      <button onClick={applyFilters} className={styles.filterButton}>Apply Filters</button>
     </div>
   );
 };
