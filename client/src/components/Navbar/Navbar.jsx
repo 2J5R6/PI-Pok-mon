e0be20a06108a497cee6ac5efabedb3d0ddc4980
Julián Andrés Rosas Sánchez <julianrosassan@gmail.com>
2023-10-30 21:02:15 -0500

    Working on components II

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import ModeToggle from '../ModeToggle/ModeToggle';
 import './Navbar.module.css';
 
 function Navbar() {
+  const location = useLocation();
+
   return (
     <nav className="navbar">
-      <Link to="/">Inicio</Link>
-      <Link to="/home">Explorar Pokémons</Link>
-      <Link to="/create">Crear Pokémon</Link>
+      <div className="nav-links">
+        <Link to="/" className={location.pathname === '/' ? 'active' : ''}>
+          Inicio
+        </Link>
+        <Link to="/home" className={location.pathname === '/home' ? 'active' : ''}>
+          Explorar Pokémons
+        </Link>
+        <Link to="/create" className={location.pathname === '/create' ? 'active' : ''}>
+          Crear Pokémon
+        </Link>
+      </div>
+      <ModeToggle />
     </nav>
   );
 }
