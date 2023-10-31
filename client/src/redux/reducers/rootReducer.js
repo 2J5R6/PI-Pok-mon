c1dc2815ec7ba1012be88643b4412f3774f33a89
Julián Andrés Rosas Sánchez <julianrosassan@gmail.com>
2023-10-31 16:00:41 -0500

    Cleenaing redux

--- client/src/redux/reducers/rootReducer.js
+++ client/src/redux/reducers/rootReducer.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 import pokemonReducer from './pokemonReducer';
-import modeReducer from './modeReducer';
+
 
 const rootReducer = combineReducers({
   pokemons: pokemonReducer,
