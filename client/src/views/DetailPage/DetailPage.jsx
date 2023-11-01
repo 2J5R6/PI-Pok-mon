8ac5890c71c3765475aeebdb1496f86ab484e965
Julián Andrés Rosas Sánchez <julianrosassan@gmail.com>
2023-11-01 17:09:20 -0500

    A way better DetailPage

--- client/src/views/DetailPage/DetailPage.jsx
+++ client/src/views/DetailPage/DetailPage.jsx
@@ -10,49 +10,49 @@ function DetailPage() {
 
   return (
     <div className={styles.container}>
-      <img src={pokemon.image} alt={pokemon.name} className={styles.image} />
-      <h1 className={styles.name}>{pokemon.name.toUpperCase()}</h1>
-      <p><strong>ID:</strong> {pokemon.id}</p>
-      <div className={styles.stat}>
-        <strong>Vida:</strong>
-        <div className={styles.progressBar}>
-          <div className={styles.progress} style={{width: `${pokemon.hp}%`}}>{pokemon.hp}</div>
+      <div className={styles.content}>
+        <img src={pokemon.image} alt={pokemon.name} className={styles.image} />
+        <h1 className={styles.name}>{pokemon.name.toUpperCase()}</h1>
+        <p><strong>ID:</strong> {pokemon.id}</p>
+        <div className={styles.stat}>
+          <strong>Vida:</strong>
+          <div className={styles.progressBar}>
+            <div className={styles.progress} style={{width: `${pokemon.hp}%`}}>{pokemon.hp}</div>
+          </div>
+        </div>
+        <div className={styles.stat}>
+          <strong>Ataque:</strong>
+          <div className={styles.progressBar}>
+            <div className={styles.progress} style={{width: `${pokemon.attack}%`}}>{pokemon.attack}</div>
+          </div>
+        </div>
+        <div className={styles.stat}>
+          <strong>Defensa:</strong>
+          <div className={styles.progressBar}>
+            <div className={styles.progress} style={{width: `${pokemon.defense}%`}}>{pokemon.defense}</div>
+          </div>
+        </div>
+        <div className={styles.stat}>
+          <strong>Velocidad:</strong>
+          <div className={styles.progressBar}>
+            <div className={styles.progress} style={{width: `${pokemon.speed}%`}}>{pokemon.speed}</div>
+          </div>
+        </div>
+        <div className={styles.stat}>
+          <strong>Altura:</strong> {pokemon.height}
+        </div>
+        <div className={styles.stat}>
+          <strong>Peso:</strong> {pokemon.weight}
+        </div>
+        <div>
+          <strong>Tipo:</strong> 
+          {pokemon.types && pokemon.types.map(type => (
+              <div key={type.id} className={styles.type}>
+                  {type.name.toUpperCase()}
+              </div>
+            ))}
         </div>
       </div>
-      <div className={styles.stat}>
-        <strong>Ataque:</strong>
-        <div className={styles.progressBar}>
-          <div className={styles.progress} style={{width: `${pokemon.attack}%`}}>{pokemon.attack}</div>
-        </div>
-      </div>
-      <div className={styles.stat}>
-        <strong>Defensa:</strong>
-        <div className={styles.progressBar}>
-          <div className={styles.progress} style={{width: `${pokemon.defense}%`}}>{pokemon.defense}</div>
-        </div>
-      </div>
-      <div className={styles.stat}>
-        <strong>Velocidad:</strong>
-        <div className={styles.progressBar}>
-          <div className={styles.progress} style={{width: `${pokemon.speed}%`}}>{pokemon.speed}</div>
-        </div>
-      </div>
-      <div className={styles.stat}>
-        <strong>Altura:</strong> {pokemon.height}
-      </div>
-      <div className={styles.stat}>
-        <strong>Peso:</strong> {pokemon.weight}
-      </div>
-
-      <div>
-        <strong>Tipo:</strong> 
-        {pokemon.types && pokemon.types.map(type => (
-            <div key={type.id} className={styles.type}>
-                {type.name.toUpperCase()}
-            </div>
-          ))}
-      </div>
-      
     </div>
   );
 }
